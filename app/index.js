9c1bc5c99bb630e0258aea01a44c92e4ee5f1873
Luís Fernando Gaido <luisfernandogaido@gmail.com>
2018-10-14 11:20:48 -0300

    scroll-linked positioning effect https://trello.com/c/0dWHOmwF

--- app/index.js
+++ app/index.js
@@ -1,7 +1,4 @@
-window.addEventListener('scroll', function() {
-  artigos.style.backgroundPositionY = ((-document.scrollingElement.scrollTop + artigos.offsetTop) / 3) + 'px';
-});
 let artigos = document.getElementById('artigos');
-setTimeout(function() {
+setInterval(function() {
   artigos.style.backgroundPositionY = ((-document.scrollingElement.scrollTop + artigos.offsetTop) / 3) + 'px';
-}, 100);
+}, 25);
