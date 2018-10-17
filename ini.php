ed6cba8beb2fee9d8f62317b3ad4ddd6be4a28c5
Luís Fernando Gaido <luisfernandogaido@gmail.com>
2018-10-17 14:42:27 -0300

    Constante SITE mais flexível ao ambiente https://trello.com/c/tfwnSE6z

--- ini.php
+++ ini.php
@@ -1,6 +1,5 @@
 <?php
 define('RAIZ', str_replace('\\', '/', __DIR__) . '/');
 include RAIZ . 'core/functions.php';
-define('SITE', $_SERVER['REQUEST_SCHEME'] . '://' . str_replace('//', '/',
-        $_SERVER['HTTP_HOST'] . '/' . str_replace([$_SERVER['DOCUMENT_ROOT'], '/'], '', RAIZ) . '/'));
+define('SITE', str_replace($_SERVER['DOCUMENT_ROOT'], '', RAIZ));
 spl_autoload_register('autoload');
